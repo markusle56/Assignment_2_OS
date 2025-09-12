24ea66da06ca70f8ff761f19f1cc8ef3a12ce93d
ahhkhoa56 <anhkhoa.wm@gmail.com>
2025-09-12 13:49:45 +0930

    upload

--- randmmu.py
+++ randmmu.py
@@ -1,7 +1,7 @@
-import random
+from mmu import MMU
 
-class RandMMU():
-    def __init__(self, frame):
+class RandMMU(MMU):
+    def __init__(self, frames):
         self.frame = []
         self.max_frame = frame
         self.page_faults = 0
@@ -9,55 +9,55 @@ class RandMMU():
         self.disk_writes = 0
         self.mode = "debug"
 
-    def setdebug(self):
+
+    def set_debug(self):
         self.mode = "debug"
 
     def reset_debug(self):
         self.mode = "quiet"
 
-    def print_debug(self, msg):
-        if self.mode == "debug":
-            print(msg)
-    def read_memory(self, pass_number):
-        if pass_number in self.frame:
-            self.print_debug(f"READ: Page {pass_number} already in frame (HIT).")
+    def read_memory(self, page_number):
+        if page_number in self.frame:
+            self.print_debug(f"READ: Page {page_number} already in frame (HIT).")
             return 
     
-        self.print_debug(f"READ: Page {pass_number} not in frame (FAULT).")
+        self.print_debug(f"READ: Page {page_number} not in frame (FAULT).")
         if len(self.frame) >= self.max_frame:
             overwrite_page = random.randint(0, self.max_frame - 1)
             self.print_debug(f"      Drop page {self.frame[overwrite_page]}")
-            self.frame[overwrite_page] = pass_number
+            self.frame[overwrite_page] = page_number
             self.disk_reads += 1
             self.page_faults += 1
         else:
-            self.frame.append(pass_number)
-            self.print_debug(f"      Loading page {pass_number} into free frame.")
+            self.frame.append(page_number)
+            self.print_debug(f"      Loading page {page_number} into free frame.")
             self.page_faults += 1
             self.disk_reads += 1 
-    def write_memory(self, pass_number):
-        if pass_number in self.frame:
-            self.print_debug(f"WRITE: Page {pass_number} already in frame (HIT).")
+
+    def write_memory(self, page_number):
+        if page_number in self.frame:
+            self.print_debug(f"WRITE: Page {page_number} already in frame (HIT).")
             return 
         
-        self.print_debug(f"WRITE: Page {pass_number} not in frame (FAULT).")
+        self.print_debug(f"WRITE: Page {page_number} not in frame (FAULT).")
         if len(self.frame) >= self.max_frame:
             overwrite_page = random.randint(0, self.max_frame - 1)
             self.print_debug(f"      Drop page {self.frame[overwrite_page]}")
-            self.frame[overwrite_page] = pass_number
+            self.frame[overwrite_page] = page_number
             self.disk_writes += 1
             self.page_faults += 1
         else:
-            self.frame.append(pass_number)
-            self.print_debug(f"      Loading page {pass_number} into free frame.")
+            self.frame.append(page_number)
+            self.print_debug(f"      Loading page {page_number} into free frame.")
             self.page_faults += 1
             self.disk_writes += 1 
 
     def get_total_disk_reads(self):
         return self.disk_reads
     
-    def get_total_dish_writes(self):
-        return self.disk_writes
-    
+
+    def get_total_disk_writes(self):
+        return self.disk_writes1
+
     def get_total_page_faults(self):
         return self.page_faults
